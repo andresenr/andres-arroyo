effe566e502a2579c82b4fe877f3e30eae96ba7c
andres arroyo <102748625+andresenr@users.noreply.github.com>
2022-10-13 11:11:49 -0500

    Update conexion.php

--- model/conexion.php
+++ model/conexion.php
@@ -2,15 +2,15 @@
 
 class conexion {
 
-    private $servidor="localhost";
-    private $usuario="root";
-    private $contrasenia="";
+    private $servidor="boya9f4ww2jhbkwn6ayd-mysql.services.clever-cloud.com";
+    private $usuario="umfucalnhiftvllt";
+    private $contrasenia="bDfyPYiUOn5c3Vnnf0p8";
     private $conexion;
 
     public function __construct() {
 
         try {
-            $this->conexion = new PDO("mysql:host=$this->servidor;dbname=frutera", $this->usuario, $this->contrasenia);
+            $this->conexion = new PDO("mysql:host=$this->servidor;dbname=boya9f4ww2jhbkwn6ayd", $this->usuario, $this->contrasenia);
             $this->conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         }
         catch (PDOException $e){
